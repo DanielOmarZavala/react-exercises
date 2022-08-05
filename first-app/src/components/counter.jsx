8d408bb3b9686a25d790e3071c47e2a264d2cfc8
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-08-05 18:57:29 -0500

    Learning ReactJS basics, creating custom login page, began writing markup foundation

---
+++ first-app/src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+export default class Counter extends Component {
+
+    //language=HTML
+    render() {
+        return (
+            <React.Fragment>
+                <h1>tester</h1>
+                <button>Increase</button>
+
+                <div className="container">
+                    <i>Login </i>
+                    <input placeholder={'Enter Email Here'}/>
+                </div>
+
+
+            </React.Fragment>
+        );
+    }
+}
+
