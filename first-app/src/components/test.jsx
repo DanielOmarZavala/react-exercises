dc3d78c36b8c0989eff57067c8e3fae0c9b33c38
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-09-24 23:44:54 -0500

    Bootstrap Grid Practice within React Component

--- first-app/src/components/test.jsx
+++ first-app/src/components/test.jsx
@@ -6,9 +6,26 @@ export default class Test extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="d-flex justify-content-center align-items-center">
-                    <h1 className="btn btn-secondary m-2">testing</h1>
-                    <div className="btn btn-secondary m-2">box</div>
+                <div className="row justify-content-center align-items-center">
+                    <div className="btn btn-secondary col-4 m-2">box 1</div>
+                    <div className="btn btn-secondary col-4 m-2">box 2</div>
+                    <div className="w-100"></div>
+                    <div className="btn btn-secondary col-4 m-2">box 3</div>
+                    <div className="btn btn-secondary col-4 m-2">box 4</div>
+                </div>
+
+                <div className="row justify-content-around align-items-center">
+                    <div className="col-11 btn btn-secondary">
+                        Nested Boxes
+                        <div className="row justify-content-around">
+                            <div className="col-4 btn btn-primary">
+                                Nested Box 1
+                            </div>
+                            <div className="col-4 btn btn-primary">
+                                Nested Box 2
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
